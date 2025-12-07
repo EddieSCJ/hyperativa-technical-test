b7c25bd9b1533cb7dc2db9a62321ded784c80be6
eddiescj <eddieprofessionalmail@gmail.com>
2025-12-07 20:55:13 -0300

    feat: create main application class for Spring Boot application

---
+++ src/main/java/com/hyperativatechtest/HyperativaTechTestApplication.java
@@ -0,0 +1,13 @@
+package com.hyperativatechtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperativaTechTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperativaTechTestApplication.class, args);
+    }
+
+}
