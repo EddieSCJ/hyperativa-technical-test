b3f4f07add5faca27f31881274a3126afe8a086e
eddiescj <eddieprofessionalmail@gmail.com>
2025-12-14 04:22:22 -0300

    feat: change id type in CardLookupResponse to UUID and add CardNotFoundException for improved error handling

---
+++ src/main/java/com/hyperativatechtest/features/card/exception/CardNotFoundException.java
@@ -0,0 +1,8 @@
+package com.hyperativatechtest.features.card.exception;
+
+public class CardNotFoundException extends RuntimeException {
+    public CardNotFoundException(String message) {
+        super(message);
+    }
+}
+
